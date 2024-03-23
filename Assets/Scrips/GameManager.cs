f6f29ff97ff71e4a54f4d9fadf29a39529a19c54
As1ary <kingpetch01@gmail.com>
2024-03-23 22:58:10 +0700

    GameManager and UpdateMat

--- Assets/Scrips/GameManager.cs
+++ Assets/Scrips/GameManager.cs
@@ -1,40 +1,127 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System;
 
 public class GameManager : MonoBehaviour
 {
+
     [SerializeField] private GameObject bowlingPrefab;
-
     [SerializeField] private GameObject bowlingPositions;
-
     [SerializeField] private GameObject pinsPrefab;
-
     [SerializeField] private GameObject[] pinsPositions;
+    [SerializeField] private TMP_Text textMeshProText;
+    [SerializeField] private Text scoreUI;
+    [SerializeField] private Text respawnCountUI;
+    [SerializeField] private Button respawnButton;
+    [SerializeField] private int maxRespawnCount = 1; // Maximum number of respawns allowed
+    private GameObject currentBowlingBall;
+    private int score = 0;
+    private int respawnCount = 0; // Number of times respawned
+    private GameObject[] pins;
+    private bool ballRespawning = false;
 
     public static GameManager instance;
 
     void Start()
     {
         instance = this;
-        SetPins(1);
-        SetPins(2);
-        SetPins(3);
-        SetPins(4);
-        SetPins(5);
-        SetPins(6);
-        SetPins(7);
-        SetPins(8);
-        SetPins(9);
-        SetPins(0);
+        SetPins();
         SetBowling();
+        pins = GameObject.FindGameObjectsWithTag("Pins");
+        respawnButton.onClick.AddListener(RespawnBowlingBall);
+        UpdateRespawnCountUI();
+        textMeshProText.text = "Respawn";
+
     }
-    private void SetPins(int i )
+
+    private void ChangeSceneButtonClicked()
     {
-        GameObject obj = Instantiate(pinsPrefab,pinsPositions[i].transform.position,Quaternion.identity);
+        throw new NotImplementedException();
+    }
+
+    void Update()
+    {
+        CountPinsDown();
+        if (respawnCount ==0)
+        {
+            textMeshProText.text = "Next";
+        }
+    }
+
+    private void SetPins()
+    {
+        for (int i = 0; i < pinsPositions.Length; i++)
+        {
+            Instantiate(pinsPrefab, pinsPositions[i].transform.position, Quaternion.identity);
+        }
     }
 
     private void SetBowling()
     {
-        GameObject obj = Instantiate(bowlingPrefab,bowlingPositions.transform.position,Quaternion.identity);
+        currentBowlingBall = Instantiate(bowlingPrefab, bowlingPositions.transform.position, Quaternion.identity);
+    }
+
+    private void CountPinsDown()
+    {
+        for(int i = 0; i < pins.Length; i++)
+        {
+            if(pins[i].transform.eulerAngles.z > 5 && pins[i].transform.eulerAngles.z < 355 && pins[i].activeSelf)
+            {
+                score++;
+                pins[i].SetActive(false);
+            }
+            scoreUI.text = score.ToString();
+        }
+        
+        if (score > 0 && !ballRespawning)
+        {
+            ballRespawning = true;
+            respawnButton.gameObject.SetActive(true);
+        }
+    }
+
+    private void UpdateRespawnButton()
+    {
+        respawnButton.gameObject.SetActive(respawnCount > 0);
+    }
+
+    private void RespawnBowlingBall()
+    {
+        if (respawnCount < maxRespawnCount)
+        {
+            respawnCount++;
+            UpdateRespawnCountUI();
+            UpdateRespawnButton();
+
+            if (currentBowlingBall != null)
+            {
+                Destroy(currentBowlingBall);
+            }
+    
+            SetBowling();
+            ballRespawning = false;
+            score = 0;
+            scoreUI.text = score.ToString();
+            foreach (GameObject pin in pins)
+            {
+                pin.SetActive(true);
+            }
+        }
+        else
+        {
+            // Change scene after the final respawn
+            SceneManager.LoadScene("EndScene");
+        }
+}
+
+    private void UpdateRespawnCountUI()
+    {
+        respawnCountUI.text = "Respawn Left: " + (maxRespawnCount - respawnCount);
     }
 }
+
+
+    
+
